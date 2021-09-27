730e98b3ac9b71551393d29844dbb6ac77b8932c
Phillip Kuhrt <mail@phi1010.com>
2021-09-27 23:34:03 +0200

    Add migration for fixed permission definition and add a test to prevent wrong definitions again.

--- src/doors/tests.py
+++ src/doors/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+import inspect
 
-# Create your tests here.
+from django.contrib.auth.models import Permission
+from django.test import TestCase
+from icecream import ic
+import django.apps
+
+from . import models
+
+
+class TestPermissionsDefinition(TestCase):
+    def setUp(self) -> None:
+        self.app_name = models.__package__
+        django_classes = set(django.apps.apps.get_models())
+        model_classes = {c for s, c in inspect.getmembers(models, inspect.isclass)}
+        classes = django_classes.intersection(model_classes)
+        self.permissions = dict()
+        for modelclass in classes:
+            metaclass = modelclass._meta
+            permissions = metaclass.permissions
+            self.permissions[modelclass] = permissions
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_permissions_on_model(self):
+        """our permissions should be defined as codename, not as appname.codename"""
+        for modelclass, permissions in self.permissions.items():
+            for codename, displayname in permissions:
+                self.assertNotIn(".", codename)
+
+    def test_permissions_in_db(self):
+        """the permissions should exist in the db"""
+        for modelclass, permissions in self.permissions.items():
+            for codename, displayname in permissions:
+                permission = Permission.objects.get(codename=codename)
+                self.assertIsNotNone(permission)
+
+    def test_wrong_permissions_not_in_db(self):
+        """the previous permissions should not exist in the db anymore"""
+        for modelclass, permissions in self.permissions.items():
+            for codename, displayname in permissions:
+                with self.assertRaises(django.contrib.auth.models.Permission.DoesNotExist):
+                    permission = Permission.objects.get(codename=self.app_name+"."+codename)
+                    ic(permission)
+
+
+    pass
