8b614b26a798ceabe7bdd96a00fe8262c901c1ef
Phillip Kuhrt <mail@phi1010.com>
2022-11-25 17:45:17 +0100

    Start mqtt for debugging

--- debug.sh
+++ debug.sh
@@ -3,6 +3,6 @@ set +eux
 echo "==== PERMANENTLY ENABLING DEBUG MODE! DO NOT USE ON PRODUCTION SERVER ===="
 touch src/data/ACTIVATE_DEBUG_MODE
 . ./secrets.env
-docker-compose -f docker-compose.yml -f docker-compose.debug.yml up opa db &
+docker-compose -f docker-compose.yml -f docker-compose.debug.yml up opa db mqtt &
 (cd src; pipenv run ./manage.py runserver)
 
