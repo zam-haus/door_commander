3715c07dfe9462043c2d4f70269f46f22809f081
Phillip Kuhrt <mail@phi1010.com>
2025-04-30 21:48:37 +0200

    Explicitly load .env file in debug.sh to make it easier to debug without "pipenv run"

--- debug.sh
+++ debug.sh
@@ -4,5 +4,6 @@ echo "==== ENABLING DEBUG MODE! DO NOT USE ON PRODUCTION SERVER ===="
 export ACTIVATE_DEBUG_MODE=actived
 . ./secrets.env
 docker-compose -f docker-compose.yml -f docker-compose.debug.yml up opa db &
+set -o allexport; source .env; set +o allexport
 (cd src; pipenv run ./manage.py runserver)
 
