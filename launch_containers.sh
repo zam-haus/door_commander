c724ac7743f4861bfb112e3eb268eed43c059914
Phillip Kuhrt <mail@phi1010.com>
2025-05-02 14:10:06 +0200

    docker compose v2 compatibility (docker-compose breaks with KeyError on Ubuntu 24.04, thus use docker compose without dash, install docker-compose-v2)

--- launch_containers.sh
+++ launch_containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euf -o pipefail
 
-COMPOSE="docker-compose"
+COMPOSE="docker compose"
 which podman-compose && COMPOSE="podman-compose"
 COMPOSE="$COMPOSE -f docker-compose.yml -f docker-compose.prod.yml"
 
@@ -9,13 +9,18 @@ test -f secrets.env
 source secrets.env
 
 set -x
+echo "======== Building ========"
 if which podman-compose ; then
   $COMPOSE build
 else
   $COMPOSE build --parallel
 fi
 # recreate the containers with the new password.
+echo "======== Recreating Containers ========"
 $COMPOSE up --no-start --force-recreate
+echo "======== Checking Django Deployment ========"
 $COMPOSE run --rm python ./manage.py check --deploy
+echo "======== Migrating Django Database ========"
 $COMPOSE run --rm python ./manage.py migrate
+echo "======== Starting Services ========"
 $COMPOSE up -d
